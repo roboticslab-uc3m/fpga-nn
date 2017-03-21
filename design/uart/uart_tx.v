51c0726d1184d236c6a7770d96f57ca72f7f4e86
Dennis <danielpintoriv@gmail.com>
2017-03-21 12:45:32 +0100

    Changes in UART_tx 	- baudrate and clock frequency are now configurables with parameters 	- Remodeled as Moore FSM

--- design/uart/uart_tx.v
+++ design/uart/uart_tx.v
@@ -3,103 +3,155 @@
 // File name   : uart_tx.v
 // Function	   : UART transmit only module
 // Engineer    : Dennis Pinto Rivero danielpintoriv@gmail.com
+//
+// Notes: Sends first the least significative bit
 //==============================================================================
 
 module uart_tx (
     input wire rst_n,
     input wire clk,
     input wire start,
-    input wire [7:0] data_in,   
+    input wire [7:0] data,   
     output reg tx,
     output reg busy
 );
 
+parameter  clock_frequency        = 12000000;
+parameter  baud_rate              = 9600;
+
+localparam clock_cycles_per_pulse = clock_frequency / baud_rate;
+
 localparam integer 
-    IDLE_ST = 2'd0,
-    SEND_START_ST = 2'd1,
-    SEND_BIT_ST = 2'd2; 
+    IDLE_st         	= 3'd0,
+	INIT_SEND_START_st 	= 3'd1,
+	WAIT_SEND_START_st 	= 3'd2,
+	INIT_SEND_BIT_st 	= 3'd3,
+	WAIT_SEND_BIT_st 	= 3'd4,
+	NEXT_BIT_st			= 3'd5;
 
-reg [1:0] state, next_state;
+reg [2:0] state, next_state;
 
-reg [7:0] data_reg;
-reg [2:0] cnt_value;
-reg [2:0] cnt_initial;
-reg cnt_ld, cnt_en;
-reg data_ld;
+reg [15:0] sync_cnt;
+reg sync_cnt_ld, sync_cnt_en;
+
+reg [2:0] bit_cnt;
+reg bit_cnt_ld, bit_cnt_en;
+
+reg [7:0] input_data;
+reg input_data_ld;
+
 
 // next state
 always @(posedge clk, negedge rst_n) begin
     if (!rst_n) begin
-        state <= IDLE_ST;
-        data_reg <= 0;
+        state <= IDLE_st;
     end else begin
         state <= next_state;
-        if (data_ld) begin
-            data_reg <= data_in;
-        end
     end
+end
+
+
+// input data
+always @(posedge clk, negedge rst_n) begin
+	if (!rst_n) begin
+		input_data <= 0;
+	end else if (input_data_ld) begin
+		input_data <= data;
+	end
 end
 
 
 // bit counter
 always @(posedge clk, negedge rst_n) begin
     if (!rst_n) begin
-        cnt_value <= 0;
-    end else if (cnt_ld) begin
-        cnt_value <= cnt_initial;
-    end else if (cnt_en) begin
-        cnt_value <= cnt_value + 1;
+        bit_cnt <= 0;
+    end else if (bit_cnt_ld) begin
+        bit_cnt <= 0;
+    end else if (bit_cnt_en) begin
+        bit_cnt <= bit_cnt + 1;
+    end
+end
+
+
+// syncronization counter
+always @(posedge clk, negedge rst_n) begin
+    if (!rst_n) begin
+        sync_cnt <= 0;
+    end else if (sync_cnt_ld) begin
+        sync_cnt <= clock_cycles_per_pulse;
+    end else if (sync_cnt_en) begin
+        sync_cnt <= sync_cnt - 1;
     end
 end
 
 
 // next state
 always @(*) begin
-    next_state <= state;
+    next_state = state;
     case (state)
-        IDLE_ST:
-            if (start) begin
-                next_state <= SEND_START_ST;
-            end
-        SEND_START_ST:
-            next_state <= SEND_BIT_ST;
-        SEND_BIT_ST:
-            if (cnt_value == 7) begin
-                next_state <= IDLE_ST;
-            end
+	 	IDLE_st:
+			if (start) begin
+				next_state = INIT_SEND_START_st;
+			end
+		INIT_SEND_START_st:
+			next_state = WAIT_SEND_START_st;
+		WAIT_SEND_START_st:
+			if (sync_cnt == 0) begin
+				next_state = INIT_SEND_BIT_st;
+			end
+		INIT_SEND_BIT_st:
+			next_state = WAIT_SEND_BIT_st;
+		WAIT_SEND_BIT_st:
+			if (sync_cnt == 0) begin
+				if (bit_cnt < 7) begin
+					next_state = NEXT_BIT_st;
+				end else if (bit_cnt == 7) begin
+					next_state = IDLE_st;
+				end
+			end
+		NEXT_BIT_st:
+			next_state = INIT_SEND_BIT_st;
     endcase 
 end
 
 
 // output logic
 always @(*) begin
-    tx = 1;
-    busy = 0;
-    data_ld = 0;
-    cnt_initial = 3'd000;
-    cnt_ld = 0;
-    cnt_en = 0;
+	tx = 1;
+	busy = 1;
+	sync_cnt_ld = 0;
+	sync_cnt_en = 0;
+
+	bit_cnt_ld = 0; 
+	bit_cnt_en = 0;
+
+	input_data_ld = 0;
+
     case (state)
-        IDLE_ST: 
-        begin
-            tx = 1;
-            busy = 1;
-            if (start) begin
-                data_ld = 1;
-            end
-        end
-        SEND_START_ST:
-        begin
-            tx = 0;
-            busy = 1;
-            cnt_ld = 1;
-        end
-        SEND_BIT_ST:
-        begin
-            tx = data_reg[cnt_value];
-            busy = 1;
-            cnt_en = 1;
-        end
+	 	IDLE_st: begin
+			busy = 0;
+		end
+		INIT_SEND_START_st: begin
+			tx = 0;
+			input_data_ld = 1;
+			bit_cnt_ld = 1;
+			sync_cnt_ld = 1;
+		end
+		WAIT_SEND_START_st: begin
+			tx = 0;
+			sync_cnt_en = 1;
+		end
+		INIT_SEND_BIT_st: begin
+			tx = input_data[bit_cnt];
+			sync_cnt_ld = 1;
+		end
+		WAIT_SEND_BIT_st: begin
+			tx = input_data[bit_cnt];
+			sync_cnt_en = 1;
+		end
+		NEXT_BIT_st: begin
+			bit_cnt_en = 1;
+		end
     endcase 
 end
 
--- test/uart_tx_tb.v
+++ test/uart_tx_tb.v
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 module uart_tx_tb;
 
 reg clk, rst_n;
@@ -5,11 +7,14 @@ reg start;
 reg [7:0] data;
 wire tx, busy;
 
+localparam clock_frequency = 12000000;
+localparam clk_pulse_width = 1000000000/clock_frequency;
+
 uart_tx uart_tx(
 	.rst_n(rst_n),
 	.clk(clk),
 	.start(start),
-	.data_in(data),
+	.data(data),
 	.tx(tx),
 	.busy(busy)
 );
@@ -21,7 +26,7 @@ initial begin
 end
 
 // Clock
-always #10 clk = !clk;
+always #(clk_pulse_width/2) clk = !clk;
 
 // Reset
 initial begin
@@ -34,18 +39,19 @@ end
 
 // testbench
 initial begin
-	start <= 0;
-	data <= 8'h00;
+	start = 0;
+	data = 8'h00;
 	wait( rst_n );
 	@(negedge clk);
 	$monitor("[Time %d]: Started testbench", $time);
 
-	data <= 8'b10101010;
-	start <= 1;
+	data = 8'b10101010;
+	start = 1;
 	
 	repeat (10) @(negedge clk);
-	start <= 0;
+	start = 0;
 
+	wait ( !busy );
 	repeat (100) @(negedge clk);
 	$finish;
 end
